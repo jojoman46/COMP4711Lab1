828993ae815560ff03c367db5bdfd0778d668f9d
Johnny <johnnymarin2011@gmail.com>
2016-01-06 11:23:28 -0800

    Used for loops in tic tac toe

--- index.php
+++ index.php
@@ -28,42 +28,78 @@
         else echo 'No winner yet.';
 
         function winner($token,$position) {
-            $won = false;
+            for($row=0; $row<3; $row++) {
+                $result = true;
+                for($col=0; $col<3; $col++)
+                    if ($position[3*$row+$col] != $token) {
+                        $result = false; // note the negative test
+                        break;
+                    }else if($col = 2){
+                        return $result;
+                    }
+            }
+            for($col=0; $col<3; $col++) {
+                $result = true;
+                for($row=0; $row<3; $row++)
+                    if ($position[3*$col+$row] != $token){
+                        $result = false; // note the negative test
+                        break;
+                    }else if($row = 2){
+                        return $result;
+                    }
+            }
+
             if (($position[0] == $token) &&
-                ($position[1] == $token) &&
-                ($position[2] == $token)) {
-                $won = true;
-            } else if (($position[3] == $token) &&
-                ($position[4] == $token) &&
-                ($position[5] == $token)) {
-                $won = true;
-            } else if (($position[6] == $token) &&
-                ($position[7] == $token) &&
-                ($position[8] == $token)) {
-                $won = true;
-            } else if (($position[0] == $token) &&
-                ($position[3] == $token) &&
-                ($position[6] == $token)) {
-                $won = true;
-            } else if (($position[1] == $token) &&
-                ($position[4] == $token) &&
-                ($position[7] == $token)) {
-                $won = true;
-            } else if (($position[2] == $token) &&
-                ($position[5] == $token) &&
-                ($position[8] == $token)) {
-                $won = true;
-            } else if (($position[0] == $token) &&
                 ($position[4] == $token) &&
                 ($position[8] == $token)) {
-                $won = true;
+                $result = true;
             }
             else if (($position[3] == $token) &&
                 ($position[4] == $token) &&
                 ($position[6] == $token)) {
-                $won = true;
+                $result = true;
             }
-            return $won;
+            return $result;
+
+
+
+
+//            $won = false;
+//            if (($position[0] == $token) &&
+//                ($position[1] == $token) &&
+//                ($position[2] == $token)) {
+//                $won = true;
+//            } else if (($position[3] == $token) &&
+//                ($position[4] == $token) &&
+//                ($position[5] == $token)) {
+//                $won = true;
+//            } else if (($position[6] == $token) &&
+//                ($position[7] == $token) &&
+//                ($position[8] == $token)) {
+//                $won = true;
+//            } else if (($position[0] == $token) &&
+//                ($position[3] == $token) &&
+//                ($position[6] == $token)) {
+//                $won = true;
+//            } else if (($position[1] == $token) &&
+//                ($position[4] == $token) &&
+//                ($position[7] == $token)) {
+//                $won = true;
+//            } else if (($position[2] == $token) &&
+//                ($position[5] == $token) &&
+//                ($position[8] == $token)) {
+//                $won = true;
+//            } else if (($position[0] == $token) &&
+//                ($position[4] == $token) &&
+//                ($position[8] == $token)) {
+//                $won = true;
+//            }
+//            else if (($position[3] == $token) &&
+//                ($position[4] == $token) &&
+//                ($position[6] == $token)) {
+//                $won = true;
+//            }
+//            return $won;
         }
 
 
