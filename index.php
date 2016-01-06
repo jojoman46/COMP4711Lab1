6a4fcd3c034ed2974d390eaad87c7d3bed1449b3
Johnny <johnnymarin2011@gmail.com>
2016-01-06 10:28:10 -0800

    Result of expressions assigned to variables

--- index.php
+++ index.php
@@ -11,6 +11,12 @@
         $level = 10;
         echo 'Hi, my name is '.$name,', and I am a level '.$level.'
         '.$what;
+        echo '<br/>';
+        $hoursworked = 10;
+        $rate = 12;
+        $total = $hoursworked * $rate;
+        echo 'You owe me '.$total;
+
         ?>
     </body>
 </html>
